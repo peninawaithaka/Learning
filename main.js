83ec8c564a82f0262a9e0e8c42c8d1a1e9b362f2
Penina Waithaka <peninawaithaka5@gmail.com>
2021-08-16 11:13:27 +0300

    Portfolio

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-const S = "Hello World";
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link')
 
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
 
-console.log(S.length);
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
